2f675d4e768277519fe2efee400446aa7808ecc8
Zehao <a15zehcheche@inspedralbes.cat>
2020-02-18 22:32:13 +0100

    Updates

---
+++ precache-manifest.89c61684bed4d287ac564495b0e2cbc0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbd60138f945a1de30e8310c746476e5",
+    "url": "/longyou/index.html"
+  },
+  {
+    "revision": "61169b15628ddae79f18",
+    "url": "/longyou/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c6e722b6869eab2ec5ec",
+    "url": "/longyou/static/js/2.35ae1b04.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/longyou/static/js/2.35ae1b04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61169b15628ddae79f18",
+    "url": "/longyou/static/js/main.c1a818c2.chunk.js"
+  },
+  {
+    "revision": "0be841c337c172a52191",
+    "url": "/longyou/static/js/runtime-main.bee49671.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/longyou/static/media/logo.5d5d9eef.svg"
+  }
+]);
