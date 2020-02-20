2021a223bbada132183b845c73a87072dbf8da1d
Zehao <a15zehcheche@inspedralbes.cat>
2020-02-20 13:45:07 +0100

    cell and card reponsive for mobile

--- longyou-app/src/component/card.js
+++ longyou-app/src/component/card.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import '../App.css';
+import Media from 'react-media';
+
 
 class Card extends Component {
+    
+    
     render() {
-        const mystyle = {
+        const pcStyle = {
             height: this.props.cardDate.height + "px",
             width: this.props.cardDate.width + "px",
             top:this.props.cardDate.top + "px",    
@@ -15,11 +19,35 @@ class Card extends Component {
             boxSizing: "border-box",
             transform: "rotate(-45deg)",
             borderRadius: "40px"
-          }
+        }
+        const mobileStyle = {
+            height: this.props.cardDate.height / 2 + "px",
+            width: this.props.cardDate.width / 2 + "px",
+            top: this.props.cardDate.top / 2 + "px",
+            left: this.props.cardDate.left / 2 + "px",
+            position: "absolute",
+            backgroundColor: "lightblue",
+            opacity: 0.5,
+            border: "3px black solid",
+            boxSizing: "border-box",
+            transform: "rotate(-45deg)",
+            borderRadius: "20px"
+        }
+    
         return (
-            <div className="cell" style={mystyle}>
-              
-            </div>
+            <Media query={{ maxWidth: 599 }}>
+            {matches =>
+                matches ? (
+                    //The document is less than 600px wide.
+                    <div className="card" style={mobileStyle}>
+                    </div>
+                ) : (
+                    //The document is at least 600px wide.
+                        <div className="card" style={pcStyle}>
+                        </div>
+                    )
+            }
+        </Media>
         );
     }
 }
--- longyou-app/src/component/cell.js
+++ longyou-app/src/component/cell.js
@@ -1,23 +1,50 @@
 import React, { Component } from 'react';
 import '../App.css';
+import Media from 'react-media';
+
+
 
 class Cell extends Component {
+
     render() {
-        const mystyle = {
+        const pcStyle = {
             height: this.props.cellDate.height + "px",
             width: this.props.cellDate.width + "px",
-            top:this.props.cellDate.top + "px",    
+            top: this.props.cellDate.top + "px",
             left: this.props.cellDate.left + "px",
             position: "absolute",
             backgroundColor: "lightblue",
             opacity: 0.5,
             border: "3px black solid",
             boxSizing: "border-box",
-          }
+        }
+        const mobileStyle = {
+            height: this.props.cellDate.height / 2 + "px",
+            width: this.props.cellDate.width / 2 + "px",
+            top: this.props.cellDate.top / 2 + "px",
+            left: this.props.cellDate.left / 2 + "px",
+            position: "absolute",
+            backgroundColor: "lightblue",
+            opacity: 0.5,
+            border: "3px black solid",
+            boxSizing: "border-box",
+        }
+
         return (
-            <div className="cell" style={mystyle}>
-              
-            </div>
+            <Media query={{ maxWidth: 599 }}>
+                {matches =>
+                    matches ? (
+                        //The document is less than 600px wide.
+                        <div className="cell" style={mobileStyle}>
+                        </div>
+                    ) : (
+                        //The document is at least 600px wide.
+                            <div className="cell" style={pcStyle}>
+                            </div>
+                        )
+                }
+            </Media>
+
         );
     }
 }
